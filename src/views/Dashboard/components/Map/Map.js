2ee7fea768a91673b06541431f82bb948b37197c
samy <s.lemaissi@hotmail.com>
2020-06-25 15:26:32 +0200

    draw polygon added

--- src/views/Dashboard/components/Map/Map.js
+++ src/views/Dashboard/components/Map/Map.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ReactMapGL from 'react-map-gl';
- 
+import {Editor, DrawPolygonMode, EditingMode} from 'react-map-gl-draw';
+
+import ControlPanel from './control-panel';
+import {getFeatureStyle, getEditHandleStyle} from './style';
 import {
   Card,
    CardContent,
@@ -23,28 +26,91 @@ import './Map.css';
       };
  
       
-        
+  _updateViewport = viewport => {
+    this.setState({viewport});
+  };
+
+  _onSelect = options => {
+    this.setState({selectedFeatureIndex: options && options.selectedFeatureIndex});
+  };
+
+  _onDelete = () => {
+    const selectedIndex = this.state.selectedFeatureIndex;
+    if (selectedIndex !== null && selectedIndex >= 0) {
+      this._editorRef.deleteFeatures(selectedIndex);
+    }
+  };
+
+  _onUpdate = ({editType}) => {
+    if (editType === 'addFeature') {
+      this.setState({
+        mode: new EditingMode()
+      });
+    }
+  };
+      _renderDrawTools = () => {
+        // copy from mapbox
+        return (
+          <div className="mapboxgl-ctrl-top-left">
+            <div className="mapboxgl-ctrl-group mapboxgl-ctrl">
+              <button
+                className="mapbox-gl-draw_ctrl-draw-btn mapbox-gl-draw_polygon"
+                title="Polygon tool (p)"
+                onClick={() => this.setState({mode: new DrawPolygonMode()})}
+              />
+              <button
+                className="mapbox-gl-draw_ctrl-draw-btn mapbox-gl-draw_trash"
+                title="Delete"
+                onClick={this._onDelete}
+              />
+            </div>
+          </div>
+        );
+      };
+    
+      _renderControlPanel = () => {
+        const features = this._editorRef && this._editorRef.getFeatures();
+        let featureIndex = this.state.selectedFeatureIndex;
+        if (features && featureIndex === null) {
+          featureIndex = features.length - 1;
+        }
+        const polygon = features && features.length ? features[featureIndex] : null;
+        return <ControlPanel containerComponent={this.props.containerComponent} polygon={polygon} />;
+      };
+
       render(){
+        const {viewport, mode} = this.state;
           return(
             <Card  >
           
             <CardContent>
               <div id="map" className="mapContainer">
-                   <ReactMapGL {...this.state.viewport} mapStyle="mapbox://styles/mapbox/outdoors-v11" onViewportChange={(viewport =>
-                   this.setState({viewport}))} mapboxApiAccessToken="pk.eyJ1Ijoic2FteWxtcyIsImEiOiJja2J1YXZvcXkwM21jMnlwZzZpOWU2aG1wIn0.E7E4xlfZunVo-RqW6fAD6Q">
+                   <ReactMapGL {...viewport} 
+                   mapStyle="mapbox://styles/mapbox/outdoors-v11" 
+                   onViewportChange={(viewport =>
+                   this.setState({viewport}))} 
+                   mapboxApiAccessToken="pk.eyJ1Ijoic2FteWxtcyIsImEiOiJja2J1YXZvcXkwM21jMnlwZzZpOWU2aG1wIn0.E7E4xlfZunVo-RqW6fAD6Q">
+                    <Editor
+                    ref={_ => (this._editorRef = _)}
+                    style={{width: '100%', height: '100%'}}
+                    clickRadius={12}
+                    mode={mode}
+                    onSelect={this._onSelect}
+                    onUpdate={this._onUpdate}
+                    editHandleShape={'circle'}
+                    featureStyle={getFeatureStyle}
+                    editHandleStyle={getEditHandleStyle}
+                    />
+                     {this._renderDrawTools()}
+        {this._renderControlPanel()}
                    </ReactMapGL>
               </div>
-              <div className="calculation-box">
-            <p>Draw a polygon using the draw tools.</p>
-            <div id="calculated-area"></div>
-            </div>
+               
             </CardContent>          
           </Card>
           )
       }
   }
- 
- 
- 
+  
 
 export default Map;
