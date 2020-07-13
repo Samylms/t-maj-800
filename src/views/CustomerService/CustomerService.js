a051f68d21a902515c657a15e9aecdc9a149c819
samy <s.lemaissi@hotmail.com>
2020-07-13 22:27:37 +0200

    chat bot added

--- src/views/CustomerService/CustomerService.js
+++ src/views/CustomerService/CustomerService.js
@@ -1,10 +1,29 @@
 import React from 'react';
- 
+import ChatBot from 'react-simple-chatbot';
+
 const CustomerService = () => {
+    const steps = [
+        {
+          id: '0',
+          message: 'Salut petit con, veux-tu connaître quel temps il fera demain ?!',
+          trigger: '1',
+        },
+        {
+            id: '1',
+            user: true,
+            trigger: '2',
+          },
+        {
+          id: '2',
+          message: 'ok... attends un peu !',
+          end: true,
+        },
+      ];
+
     return (
-       <div>
-          <h1>Customer Service</h1>
-           <p>Customer Service page body content</p>
+       <div style={{display: 'flex', justifyContent: 'center', marginTop: '7%'}}>
+         
+          <ChatBot steps={steps} />
        </div>
     );
 }
