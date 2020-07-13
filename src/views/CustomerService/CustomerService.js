88b65a49a090a88cdf4781e45b8275ecd78c1eee
DataTriny <datatriny@gmail.com>
2020-07-13 21:39:30 +0200

    Rename Costumer service

---
+++ src/views/CustomerService/CustomerService.js
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+const CustomerService = () => {
+    return (
+       <div>
+          <h1>Customer Service</h1>
+           <p>Customer Service page body content</p>
+       </div>
+    );
+}
+ 
+export default CustomerService;
