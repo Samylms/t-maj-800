fd7a095fa472353f7a79800a4d8e2cd570f9c03a
natdev <dordonne.nathanael@gmail.com>
2020-07-13 18:42:58 +0200

    correct sidebar

--- src/views/Sidebar/Appbar.js
+++ src/views/Sidebar/Appbar.js
@@ -11,6 +11,7 @@ import IconButton from '@material-ui/core/IconButton';
  import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
  import Sideitem  from    './Sideitem';
+ import '../../style.css';
   
  
 
